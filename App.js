4ed3988f9617904736a8499778cac11ec16f0064
epalacio <eduardo.palacio@nemesys.com.x>
2019-02-26 19:53:39 -0600

    Screens and navigation bar

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import MapScreen from './screens/MapScreen';
+import AboutScreen from './screens/AboutScreen';
+import AuthScreen from './screens/AuthScreen';
+import ComplaintScreen from './screens/ComplaintScreen';
+
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+import { Icon } from 'react-native-elements';
+
+
+
+const store = createStore(reducers, { },
+    compose(
+        applyMiddleware(thunk)
+    )
+);
+
+
+const MainNavigator = createBottomTabNavigator({
+
+        map: {
+            screen: createStackNavigator({
+                map: { screen: MapScreen },
+                complaint: { screen: ComplaintScreen },
+                auth: { screen: AuthScreen }
+            })
+            ,  navigationOptions:{
+                title:'Mapa',
+                tabBarIcon : ({tintColor}) => {
+                    return <Icon name='my-location' size={30} color={tintColor}/>
+                }
+            }},
+        about:{ screen: AboutScreen, navigationOptions: {
+                title:'Acerca de',
+                tabBarIcon : ({tintColor}) => {
+                    return <Icon name='question'  type='antdesign' size={30} color={tintColor}/>
+                }
+            } }
+
+
+
+    },
+    {
+        navigationOptions: () => ({
+
+
+        })
+    }
+);
+
+
+const AppContainer = createAppContainer(MainNavigator);
+
+
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Provider store={ store }>
+
+            <AppContainer />
+        </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ screens/ComplaintScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ComplaintScreen extends Component {
+
+    render(){
+        return(
+            <View>
+                <Text>ComplaintScreen</Text>
+                <Text>ComplaintScreen</Text>
+                <Text>ComplaintScreen</Text>
+                <Text>ComplaintScreen</Text>
+                <Text>ComplaintScreen</Text>
+                <Text>ComplaintScreen</Text>
+                <Text>ComplaintScreen</Text>
+                <Text>ComplaintScreen</Text>
+            </View>
+        )
+    }
+}
+
+export default ComplaintScreen;
